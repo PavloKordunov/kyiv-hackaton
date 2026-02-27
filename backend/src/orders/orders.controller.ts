aa60ae601190bcad9de8c0ea804fb8e8d1729efa
GontarRoma <romagontar989@gmail.com>
2026-02-28 00:27:27 +0200

    add GET endpoint for orders with filters and pagination

--- backend/src/orders/orders.controller.ts
+++ backend/src/orders/orders.controller.ts
@@ -4,13 +4,21 @@ import {
   UseInterceptors,
   UploadedFile,
   BadRequestException,
+  Get,
+  Query,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { OrdersService } from './orders.service';
+import { GetOrdersFilterDto } from './dto/getOrdersFilterDto';
 
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
+
+  @Get()
+  async getOrders(@Query() query: GetOrdersFilterDto) {
+    return this.ordersService.getOrders(query);
+  }
 
   @Post('import')
   @UseInterceptors(FileInterceptor('file'))
