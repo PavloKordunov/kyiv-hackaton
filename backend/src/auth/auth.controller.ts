b2a050498fd8af195d7ab7ea14b0d9eaa1b64251
PavloKordunov <102924208+PavloKordunov@users.noreply.github.com>
2026-02-28 23:42:30 +0200

    cookie settings

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -6,26 +6,35 @@ import { JwtGuard } from './jwtGuard';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService:AuthService){}
+  constructor(private readonly authService: AuthService) {}
 
-    @Post('login')
-    async login(@Body() dto:loginDto,
-    @Res({passthrough:true}) res){
-    const {user,token}=await this.authService.login(dto)
-    res.cookie('user_token',token.accessToken,{
-        httpOnly:true,
-        secure:process.env.NODE_ENV==='production',
-        sameSite:'lax',
-        expires:new Date(Date.now()+3600000)
-    })
-        return {message:'successful login',user}
-    }
+  @Post('login')
+  async login(
+    @Body() dto: loginDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { user, token } = await this.authService.login(dto);
 
-    @UseGuards(JwtGuard)
-    @Post('logout')
-    
-    async logout(@Res({passthrough:true}) res:Response){
-        res.cookie('user_token','',{expires:new Date(0)})
-        return {message:'користувач успішно вийшов'}
-    }
+    res.cookie('user_token', token.accessToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none',
+      expires: new Date(Date.now() + 3600000),
+    });
+
+    return { message: 'successful login', user };
+  }
+
+  @UseGuards(JwtGuard)
+  @Post('logout')
+  async logout(@Res({ passthrough: true }) res: Response) {
+    res.cookie('user_token', '', {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none',
+      expires: new Date(0),
+    });
+
+    return { message: 'користувач успішно вийшов' };
+  }
 }
